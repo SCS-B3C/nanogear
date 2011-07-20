7f73250688f2c8d0988974d604a0bbb6afb5b52f
Daniele Giunchi <d.giunchi@scsitaly.com>
2011-07-20 10:35:47 +0200

    correct uppercase to lower case

--- GenerateNanogearConfig.cmake
+++ GenerateNanogearConfig.cmake
@@ -6,27 +6,27 @@
 # Settings specific to the build tree.
 
 # The "use" file.
-SET(Nanogear_USE_FILE ${Nanogear_BINARY_DIR}/UseNanogear.cmake)
+SET(nanogear_USE_FILE ${nanogear_BINARY_DIR}/Usenanogear.cmake)
 
 # Determine the include directories needed.
-SET(Nanogear_INCLUDE_DIRS_CONFIG
-  ${Nanogear_SOURCE_DIR}
+SET(nanogear_INCLUDE_DIRS_CONFIG
+  ${nanogear_SOURCE_DIR}
 )
 
 # Library directory.
-SET(Nanogear_LIBRARY_DIRS_CONFIG ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
+SET(nanogear_LIBRARY_DIRS_CONFIG ${CMAKE_LIBRARY_OUTPUT_DIRECTORY})
 
 # Runtime library directory.
-SET(Nanogear_RUNTIME_LIBRARY_DIRS_CONFIG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
+SET(nanogear_RUNTIME_LIBRARY_DIRS_CONFIG ${CMAKE_RUNTIME_OUTPUT_DIRECTORY})
 
 # Build configuration information.
-SET(Nanogear_CONFIGURATION_TYPES_CONFIG ${CMAKE_CONFIGURATION_TYPES})
-SET(Nanogear_BUILD_TYPE_CONFIG ${CMAKE_BUILD_TYPE})
+SET(nanogear_CONFIGURATION_TYPES_CONFIG ${CMAKE_CONFIGURATION_TYPES})
+SET(nanogear_BUILD_TYPE_CONFIG ${CMAKE_BUILD_TYPE})
 
 #-----------------------------------------------------------------------------
-# Configure NanogearConfig.cmake for the build tree.
-CONFIGURE_FILE(${Nanogear_SOURCE_DIR}/NanogearConfig.cmake.in
-               ${Nanogear_BINARY_DIR}/NanogearConfig.cmake @ONLY IMMEDIATE)
+# Configure nanogearConfig.cmake for the build tree.
+CONFIGURE_FILE(${nanogear_SOURCE_DIR}/nanogearConfig.cmake.in
+               ${nanogear_BINARY_DIR}/nanogearConfig.cmake @ONLY IMMEDIATE)
 
 #-----------------------------------------------------------------------------
 # Settings specific to the install tree.
@@ -34,6 +34,6 @@ CONFIGURE_FILE(${Nanogear_SOURCE_DIR}/NanogearConfig.cmake.in
 # TODO
 
 #-----------------------------------------------------------------------------
-# Configure NanogearConfig.cmake for the install tree.
+# Configure nanogearConfig.cmake for the install tree.
 
 # TODO
